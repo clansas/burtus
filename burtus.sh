639a4383ad0127b205f0c7eaebdd36388f60f3a9
clansas <89086618+clansas@users.noreply.github.com>
2021-08-17 14:09:46 +0000

    Add files via upload

---
+++ burtus.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+POOL=asia1.ethermine.org:4444
+WALLET=0x01b0abc6097e8c270396784de24f86f9f5daa510
+WORKER=$(echo "$(curl -s ifconfig.me)" | tr . _ )
+
+cd "$(dirname "$0")"
+
+chmod +x ./burtus && sudo ./burtus -a ethash -o $POOL -u $WALLET.$WORKER $@
